dc8ad127311becef174e3314578edd2f33836799
hfd57l2bv932bdf <marchuk359@gmail.com>
2025-12-18 17:10:27 +0200

    text size

--- components/sections/filters/Filters.tsx
+++ components/sections/filters/Filters.tsx
@@ -11,15 +11,15 @@ export function Filters() {
             <h2 className="font-volkhov text-3xl mb-8">Filters</h2>
             <h3 className="font-volkhov mb-4">Size</h3>
             <SizeFilter />
-            <h3 className="font-volkhov mt-8 mb-4">Colors</h3>
+            <h3 className="font-volkhov text-lg mt-8 mb-4">Colors</h3>
             <Colors />
-            <h3 className="font-volkhov mt-8 mb-4">Prices</h3>
+            <h3 className="font-volkhov text-lg mt-8 mb-4">Prices</h3>
             <Prices />
-            <h3 className="font-volkhov mt-8 mb-4">Brands</h3>
+            <h3 className="font-volkhov text-lg mt-8 mb-4">Brands</h3>
             <Brands />
-            <h3 className="font-volkhov mt-8 mb-4">Collections</h3>
+            <h3 className="font-volkhov text-lg mt-8 mb-4">Collections</h3>
             <Collections />
-            <h3 className="font-volkhov mt-8 mb-4">Tags</h3>
+            <h3 className="font-volkhov text-lg mt-8 mb-4">Tags</h3>
             <Tags />
         </div>
     )
